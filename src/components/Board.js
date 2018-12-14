c986ab2106d8e1ba9b96919b7c3007559e07a635
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-12-13 17:45:29 -0800

    get request

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,18 +8,61 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      board: '',
       cards: [],
+      errorMessage: ''
     };
+
+
+}
+  componentDidMount() {
+    const url = this.props.url;
+    const board = this.props.boardName;
+    //call axios after mounted
+    axios.get(url + '/' + board + '/cards')
+        .then((response) => {
+          console.log('response', response);
+          const cards = response.data.map((card) => {
+            const newCard = {...card};
+            return newCard;
+          });
+
+          this.setState({
+            cards,
+          })
+        })
+        .catch((error) => {
+          console.log('errors:', error.message);
+          this.setState ({
+            errorMessage: error.message
+          });
+        });
   }
 
+  handleUpdate = (id) => {
+    console.log('update card with id:', id);
+  };
+
+  handleDelete = (id) => {
+    console.log('delete card with id:', id);
+  };
+
   render() {
+    const cardList = this.state.cards.map((card) => {
+      return <Card key={card.id}
+                   id={card.id}
+                   removeCardCallback={this.handleDelete}
+                   updateCardCallback={this.handleUpdate}
+                   {...card} />
+    });
+    console.log('Cardlist:', cardList);
     return (
       <div>
-        Board
+        { cardList }
       </div>
     )
   }
@@ -27,7 +70,8 @@ class Board extends Component {
 }
 
 Board.propTypes = {
-
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string,
 };
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,17 +4,42 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-class Card extends Component {
-  render() {
+// class Card extends Component {
+const Card = (props) => {
+  const { id, text, emoji } = props.card;
+  // render() {
+    console.log(props);
     return (
-      <div className="card">
-        Card
+      <div className="card" key={id}>
+          <section className="card__content">
+              <p className="card__content-text"> {text} </p>
+              <p className="card__content-emoji"> {emoji} </p>
+          </section>
+        <button
+            onClick={props.updateCardCallback(props.id)}
+            type="button"
+            className="card__update"
+            aria-label="Update"
+        > UPDATE
+        </button>
+        <button
+            onClick={props.removeCardCallback(props.id)}
+            type="button"
+            className="card__delete"
+            aria-label="Remove"
+        > REMOVE
+        </button>
       </div>
-    )
-  }
-}
+    );
+  // }
+};
 
 Card.propTypes = {
+  id: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  emoji: PropTypes.object,
+  updateCardCallback: PropTypes.func,
+  deleteCardCallback: PropTypes.func,
 
 };
 
