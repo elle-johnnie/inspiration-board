d55f564af7e0f6a9b7ced778c889905c69c9bd7c
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-12-15 11:56:33 -0800

    Updates

---
+++ precache-manifest.57591bccfde7d85b2e7f902ff6ad2b9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5decc4f26d46d7cc72e",
+    "url": "/inspiration-board/static/css/main.2083e8b0.chunk.css"
+  },
+  {
+    "revision": "d5decc4f26d46d7cc72e",
+    "url": "/inspiration-board/static/js/main.d5decc4f.chunk.js"
+  },
+  {
+    "revision": "757fe26bb3736c505b8b",
+    "url": "/inspiration-board/static/js/1.757fe26b.chunk.js"
+  },
+  {
+    "revision": "d769499afd0cba5bf1c2",
+    "url": "/inspiration-board/static/js/runtime~main.d769499a.js"
+  },
+  {
+    "revision": "8b40e5dc97785c7d11fb5589a0bbddfa",
+    "url": "/inspiration-board/index.html"
+  }
+];
